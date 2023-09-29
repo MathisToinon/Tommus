907145f677438405836ad5f3af3ed40bd9d82835
Mathis Toinon <toinon.math@gmail.com>
2023-09-29 17:21:15 +0200

    Nouveau composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
 
 import { AppComponent } from './app.component';
 import { AssignmentComponent } from './assignment/assignment.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { AssignmentDetailComponent } from './assignment/assignment-detail/assignment-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AssignmentComponent
+    AssignmentComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    AssignmentDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
